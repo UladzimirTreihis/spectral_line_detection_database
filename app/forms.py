553a98f3ebc427584f37c6a21348760fba487268
virtuoso98 <e0014867@u.nus.edu>
2021-01-31 17:34:38 +0800

    deleted base urls and integrated flask with react. Working on flask forms and database now
    Small change here. I removed all base links and linked react (in local folder) to flask. Will update file structure later on

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,3 @@
-from app import app
-from flask import render_template
-
-@app.route("/")
-def login():
-    return render_template("./auth/login.html")
-
-@app.route("/home")
-def home():
-    return "home"
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
