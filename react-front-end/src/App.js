c1e1a03f5e870617673dda755d3b215b92e7cee3
virtuoso98 <e0014867@u.nus.edu>
2021-02-01 09:48:30 +0800

    put front end and back end in same directory

---
+++ react-front-end/src/App.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import './App.css';
+// import { useEffect, useState } from 'react';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
