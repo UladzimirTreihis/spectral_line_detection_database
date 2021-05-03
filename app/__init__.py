0704e8d04bc70d800747f1b91c5b4f65dab1f5d5
UladzimirTreihis <uladzimir.treihis@gmail.com>
2021-05-03 23:13:07 +0800

    Changed to one line

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import forms
-from app import models
+from app import forms, models
+
 
