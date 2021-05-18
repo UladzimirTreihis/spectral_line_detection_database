107cc653ade8832dd49a9cbe110e9d5b3d61f81e
UladzimirTreihis <uladzimir.treihis@gmail.com>
2021-05-18 11:42:10 +0800

    Added user profiles, deleted temporary solutions to login, resolved non-appearing flash messages

--- flask-back-end/app/routes.py
+++ flask-back-end/app/routes.py
@@ -1,10 +1,17 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
-from app.forms import RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+#The last-seen functionality (if necessary), otherwise it could still be usefull of any before_request functionality
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 #default route
 @app.route("/")
@@ -57,10 +64,31 @@ def register():
         return redirect(url_for('login'))
     return render_template('./auth/register.html', title= 'Register', form=form)
 
-#temporal log-in redirection route
-@app.route("/login_successful")
-def login_successful():
-    return render_template("./auth/login_successful.html")
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form, user=user)
 
 #route for galaxy/line form submission, the form yet to be developed    
 @app.route("/submit")
