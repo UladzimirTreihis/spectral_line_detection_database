ec2eabde16fff9447d5101e87d7418ae9dafa0f0
UladzimirTreihis <uladzimir.treihis@gmail.com>
2021-05-20 14:39:27 +0800

    Handled some errors, created custom html templates for errors, configured logs

--- flask-back-end/app/__init__.py
+++ flask-back-end/app/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import *
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 
 #create the instance of flask app
@@ -15,6 +18,30 @@ elif app.config["ENV"] == "testing":
     app.config.from_object("config.TestingConfig")
 else:
     app.config.from_object("config.DevelopmentConfig")
+
+if not app.debug:
+    #The email configuration for errors does not work yet on external mails such as gmail.com. works only internally on imaginary email servise on a separate localhost. 
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure()
+        mail_handler = SMTPHandler(mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']), fromaddr='no-reply@' + app.config['MAIL_SERVER'], toaddrs=app.config['ADMINS'], subject='Datavase Failure', credentials=auth, secure=secure) 
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    #Logs files function properly
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/database.log', maxBytes=10240, backupCount=10)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('App startup')
 
 #the instance and migration of db
 db = SQLAlchemy(app)
--- flask-back-end/app/forms.py
+++ flask-back-end/app/forms.py
@@ -31,3 +31,14 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    #Yet to understand the method
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+    #if the username has changed to something but original_uername, then query if new username is not in the database, if it is, raise ValidationError
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username')
