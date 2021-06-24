ccbe30734c4b658b9f35b6821cd10ad53bd1af02
akanksha-chokshi <akankshachokshi@gmail.com>
2021-06-24 21:17:32 +0800

    ability to select to approve

--- flask-back-end/app/adm/__init__.py
+++ flask-back-end/app/adm/__init__.py
@@ -1,33 +1,28 @@
-from flask import Blueprint, url_for, redirect
+from flask import Blueprint, url_for, redirect, flash
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import expose
 from flask_admin.model.template import EndpointLinkRowAction
+from flask_admin.actions import action
 from app.models import User, Galaxy, Line, TempGalaxy, TempLine
 from app import admin, db
+
 
 bp = Blueprint('adm', __name__)
 
 class TempGalaxyView(ModelView):
-    column_extra_row_actions = [  # Add a new action button
-        EndpointLinkRowAction("glyphicon glyphicon-ok", ".approve_view"),
-    ]
-
-    @expose("/approve", methods=("GET",))
-    def approve_view(self):
-        return redirect (url_for ('tempgalaxy.index_view'))
+    @action('approve', 'Approve')
+    def action_approve(self, ids):
+        pass
+        
 
 class TempLineView(ModelView):
-    column_extra_row_actions = [  # Add a new action button
-        EndpointLinkRowAction("glyphicon glyphicon-ok", ".approve_view"),
-    ]
-
-    @expose("/approve", methods=("GET",))
-    def approve_view(self):
-        return redirect (url_for ('templine.index_view'))
+    @action('approve', 'Approve')
+    def action_approve(self, ids):
+        pass
 
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Galaxy, db.session))
 admin.add_view(ModelView(Line, db.session))
-admin.add_view(TempGalaxyView (TempGalaxy, db.session))
-admin.add_view(TempLineView (TempLine, db.session))
+admin.add_view(TempGalaxyView (TempGalaxy, db.session, category = "New Entries"))
+admin.add_view(TempLineView(TempLine, db.session, category = "New Entries"))
 
